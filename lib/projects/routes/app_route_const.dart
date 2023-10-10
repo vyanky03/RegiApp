efcf2d76289d829a3231d5c8a8b1dcbcfe2dd5af
Vyankatesh Kshatriya <vyankatesh.kshatriya@gmail.com>
2023-10-10 14:06:24 +0530

    update

--- lib/projects/routes/app_route_const.dart
+++
@@ -1,5 +0,0 @@
-class MyAppRouteConstants {
-  static const String homeRouteName = 'home';
-  static const String loginpageRouteName = 'firstpage';
-  static const String signuppageRouteName = 'secondpage';
-}
